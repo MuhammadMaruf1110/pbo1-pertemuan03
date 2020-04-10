889c6023fe3a108194d9b4221e90fbd5f8095a5a
AhmadBijarmi25 <Bijarmi99@gmail.com>
2020-04-10 21:03:47 +0700

    Operator Binary Assigment

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Muhammad Maruf";
     int jumlahsks = 24;
     double indeksPrestasikumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
