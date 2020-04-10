21f096be27b154f920d083a01c7d0cbfacf0b850
AhmadBijarmi25 <Bijarmi99@gmail.com>
2020-04-10 20:41:53 +0700

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assisgment/penugasan
+    //menempatkan nilai ke dalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Maruf";
+    int jumlahsks = 24;
+    double indeksPrestasikumulatif = 3.89;
+}
