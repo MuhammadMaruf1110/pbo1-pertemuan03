8ba9a20fd497b7a01e6465cddd2da2f3097e60c2
AhmadBijarmi25 <Bijarmi99@gmail.com>
2020-04-10 22:35:44 +0700

    Switch java

---
+++ src/percabangkan/IFelse.java
@@ -0,0 +1,14 @@
+package percabangkan;
+
+public class IFelse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else {
+            System.out.println("uang cukup, berelaan lah");
+        }
+    }
+}
---
+++ src/percabangkan/Switch.java
@@ -0,0 +1,26 @@
+package percabangkan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("kecamatan anjir muara");
+                break;
+            case 2:
+                System.out.println("urang tamban");
+                break;
+            case 3:
+                System.out.println("sungai barito");
+                break;
+            case 4:
+                System.out.println("Kelotok mengangkut barang");
+                break;
+            case 5:
+                System.out.println("binian kembang desa bungas bungas");
+                break;
+        }
+    }
+}
+}
+
